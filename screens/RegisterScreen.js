773a9a0540ada985f353c50dd1910c7fddcac50a
akg4545 <abhaygupta565642@gmail.com>
2025-06-21 19:08:18 +0530

    fix RegisterScreen.js semicolon issue

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', padding: 20 },
   title: { fontSize: 24, marginBottom: 20 },
   input: { borderWidth: 1, padding: 10, marginBottom: 10 },
-})
+});
