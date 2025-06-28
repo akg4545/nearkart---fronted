0711bbfba0bbcd68894d1ee1f27c3c6c16b40121
akg4545 <abhaygupta565642@gmail.com>
2025-06-28 23:50:38 +0530

    feat: setup React Navigation with Login, Register, OTP, Home, Cart and Orders screens

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+// App.js
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import PhoneAuthScreen from './screens/PhoneAuthScreen';
 import HomeScreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
 import OrdersScreen from './screens/OrdersScreen';
@@ -16,6 +19,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="PhoneAuth" component={PhoneAuthScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Cart" component={CartScreen} />
         <Stack.Screen name="Orders" component={OrdersScreen} />
